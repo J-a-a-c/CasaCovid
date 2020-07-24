bdbbe3a79e0ec9afdf54fb5e0753e50946534d5d
J-a-a-c <jaac.alc@gmail.com>
2020-07-23 23:17:46 -0500

    agregados perfil paciente y perfil doctor

---
+++ app/src/main/java/com/example/casacovid/perfil_paciente.java
@@ -0,0 +1,14 @@
+package com.example.casacovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class perfil_paciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_paciente);
+    }
+}
