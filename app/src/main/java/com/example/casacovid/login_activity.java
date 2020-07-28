3f9926801239e6ff224e262f7d09ade149980a8d
J-a-a-c <jaac.alc@gmail.com>
2020-07-28 00:08:22 -0500

    boton3

--- app/src/main/java/com/example/casacovid/login_activity.java
+++ app/src/main/java/com/example/casacovid/login_activity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class login_activity extends AppCompatActivity {
@@ -16,11 +17,20 @@ public class login_activity extends AppCompatActivity {
         setContentView(R.layout.activity_login_activity);
         getSupportActionBar().hide();
 
+        TextView gotoRegister=(TextView)findViewById(R.id.gotoRegister);
+        gotoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), Registro_activity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), Registro_activity.class);
+                Intent intent = new Intent (v.getContext(), lineas_emergencia.class);
                 startActivityForResult(intent, 0);
             }
         });
