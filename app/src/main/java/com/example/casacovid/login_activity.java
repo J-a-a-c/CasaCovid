0396aaa2cf02b7ecc72612ef44f4fbe3cc7e802c
FLORESMARIE18 <44067536+FLORESMARIE18@users.noreply.github.com>
2020-07-27 23:01:53 -0500

    a

--- app/src/main/java/com/example/casacovid/login_activity.java
+++ app/src/main/java/com/example/casacovid/login_activity.java
@@ -3,6 +3,10 @@ package com.example.casacovid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
 
 public class login_activity extends AppCompatActivity {
 
@@ -11,5 +15,17 @@ public class login_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
         getSupportActionBar().hide();
+
+        Button btnRegistro = findViewById(R.id.btnRegistro);
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), Registro_activity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+
     }
 }
