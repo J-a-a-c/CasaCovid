4ba09aa1fe1e6c870ee6ee3cff5ee8aca717bb84
J-a-a-c <jaac.alc@gmail.com>
2020-07-28 03:38:46 -0500

    llamadas funcionan

--- app/src/main/java/com/example/casacovid/lineas_emergencia.java
+++ app/src/main/java/com/example/casacovid/lineas_emergencia.java
@@ -1,14 +1,36 @@
 package com.example.casacovid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class lineas_emergencia extends AppCompatActivity {
 
+    Button
+    btnLlamar1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lineas_emergencia);
+        btnLlamar1=(Button) findViewById(R.id.btnLlamar1);
+
+        btnLlamar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(Intent.ACTION_CALL, Uri.parse("tel:113"));
+                if(ActivityCompat.checkSelfPermission(lineas_emergencia.this, Manifest.permission.CALL_PHONE)!=
+                        PackageManager.PERMISSION_GRANTED)
+                    return;
+                startActivity(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/casacovid/login_activity.java
+++ app/src/main/java/com/example/casacovid/login_activity.java
@@ -1,7 +1,11 @@
 package com.example.casacovid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
@@ -11,11 +15,14 @@ import android.widget.TextView;
 
 public class login_activity extends AppCompatActivity {
 
+    final private int REQUEST_CODE_ASK_PERMISSION=111;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
         getSupportActionBar().hide();
+
+        solicitarPermisos();
 
         TextView gotoRegister=(TextView)findViewById(R.id.gotoRegister);
         gotoRegister.setOnClickListener(new View.OnClickListener() {
@@ -35,4 +42,16 @@ public class login_activity extends AppCompatActivity {
             }
         });
     }
+
+    private void solicitarPermisos() {
+        int permisoCall = ActivityCompat.checkSelfPermission(login_activity.this, Manifest.permission.CALL_PHONE);
+
+        if (permisoCall!= PackageManager.PERMISSION_GRANTED){
+
+            if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},REQUEST_CODE_ASK_PERMISSION);
+            }
+        }
+    }
+
 }
