71d3a643fb0e987b0974c99a19a53e9a8d0e1861
FLORESMARIE18 <44067536+FLORESMARIE18@users.noreply.github.com>
2020-07-27 22:30:53 -0500

    a

---
+++ app/src/main/java/com/example/casacovid/lineas_emergencia.java
@@ -0,0 +1,14 @@
+package com.example.casacovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lineas_emergencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lineas_emergencia);
+    }
+}
