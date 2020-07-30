dad593759bf417f67882349a4709afbc1a98eb5b
casey96gh <68678423+casey96gh@users.noreply.github.com>
2020-07-30 02:12:12 -0500

    Casey S--datosDoctor  y Emergencia

---
+++ app/src/main/java/com/example/casacovid/datosDoctor.java
@@ -0,0 +1,14 @@
+package com.example.casacovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class datosDoctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_doctor);
+    }
+}
