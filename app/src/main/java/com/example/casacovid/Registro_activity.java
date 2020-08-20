56b7310831a2b2dbb95b75aaa6ea26ce17e771de
J-a-a-c <jaac.alc@gmail.com>
2020-08-20 16:34:46 -0500

    login

--- app/src/main/java/com/example/casacovid/Registro_activity.java
+++ app/src/main/java/com/example/casacovid/Registro_activity.java
@@ -1,10 +1,28 @@
 package com.example.casacovid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registro_activity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+
+    private EditText correo;
+    private EditText contrasena;
+    private EditText contrasenaConfirmacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +31,67 @@ public class Registro_activity extends AppCompatActivity {
         getSupportActionBar().hide();
 
 
+        mAuth = FirebaseAuth.getInstance();
+
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contrasena);
+        contrasenaConfirmacion = findViewById(R.id.contrasenaConfirmacion);
+
+
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
+    public void registrarUsuario (View view){
+
+
+        if (contrasena.getText().toString().equals(contrasenaConfirmacion.getText().toString())){
+
+
+            mAuth.createUserWithEmailAndPassword(correo.getText().toString(), contrasena.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(getApplicationContext(), "Usuario Registrado.", Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent i = new Intent(getApplicationContext(), menu.class);
+                                startActivity(i);
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+
+
+
+        }else {
+
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+
+
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/casacovid/login_activity.java
+++ app/src/main/java/com/example/casacovid/login_activity.java
@@ -1,5 +1,6 @@
 package com.example.casacovid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -10,10 +11,23 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class login_activity extends AppCompatActivity {
+
+    private EditText correo;
+    private EditText contrasena;
+
+    private FirebaseAuth mAuth;
 
     final private int REQUEST_CODE_ASK_PERMISSION=111;
     @Override
@@ -21,6 +35,14 @@ public class login_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
         getSupportActionBar().hide();
+
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contrasena);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
 
         solicitarPermisos();
 
@@ -33,15 +55,46 @@ public class login_activity extends AppCompatActivity {
             }
         });
 
-        Button btnLogin = (Button) findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), menu.class);
-                startActivityForResult(intent, 0);
-            }
-        });
+
     }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void iniciarSesion (View view){
+        mAuth.signInWithEmailAndPassword(correo.getText().toString(), contrasena.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(getApplicationContext(), menu.class);
+                            startActivity(i);
+                            Toast.makeText(getApplicationContext(), "Autenticación correcta.", Toast.LENGTH_SHORT).show();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+
+
+
+
+
+    }
+
 
     private void solicitarPermisos() {
         int permisoCall = ActivityCompat.checkSelfPermission(login_activity.this, Manifest.permission.CALL_PHONE);
